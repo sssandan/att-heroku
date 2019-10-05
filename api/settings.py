ab9e0339f0ea508cc03c58afb7a002815999de77
ryan <main@zaddys-MacBook-Pro.local>
2019-10-04 21:48:00 -0700

    initial commit

--- api/settings.py
+++ api/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'notable_django.urls'
+ROOT_URLCONF = 'notabledjango.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'notable_django.wsgi.application'
+WSGI_APPLICATION = 'notabledjango.wsgi.application'
 
 
 # Database
